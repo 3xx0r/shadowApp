6f52f9303c9555af556f5b64365d91f60e489aba
TXT - 16 <txt-16@guest-wireless-207-151-035-005.usc.edu>
2018-07-12 14:15:37 -0700

    Test 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
+import { StatusBar } from '@ionic-native/status-bar';
+import { Platform } from 'ionic-angular';
 import { TabsPage } from '../pages/tabs/tabs';
 import { ConnectPage } from '../pages/connect/connect';
+
 
 @Component({
   templateUrl: 'app.html'
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
-import {ConnectPage} from '../pages/connect/connect';
+import { ConnectPage } from '../pages/connect/connect';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
